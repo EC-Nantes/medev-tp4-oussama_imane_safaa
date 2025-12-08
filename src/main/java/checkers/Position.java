e228ced09b755e959e7e84a72ec0d893ec862788
Laasri Imane <imanelaasri23@gmail.com>
2025-12-08 15:49:30 +0100

    commit de Board et Position

---
+++ src/main/java/checkers/Position.java
@@ -0,0 +1,15 @@
+package checkers;
+
+public class Position {
+    public int row;
+    public int col;
+
+    public Position(int r, int c) {
+        row = r;
+        col = c;
+    }
+
+    public static Position midpoint(Position a, Position b) {
+        return new Position((a.row + b.row) / 2, (a.col + b.col) / 2);
+    }
+}
