257cc151b0c03a8dd4f1779e59fa53a8cffec55b
Bz Safae <bouzidisafa84@gmail.com>
2025-12-08 15:45:16 +0100

    Classe Position et Move

---
+++ src/main/java/checkers/Position.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package checkers;
+
+/**
+ *
+ * @author dodi
+ */
+public class Position {
+    public int row;
+    public int col;
+
+    public Position(int r, int c) {
+        row = r;
+        col = c;
+    }
+
+    public static Position midpoint(Position a, Position b) {
+        return new Position((a.row + b.row) / 2, (a.col + b.col) / 2);
+    }
+}
