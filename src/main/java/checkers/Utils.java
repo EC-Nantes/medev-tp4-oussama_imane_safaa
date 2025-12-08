b240ad8a819d6a05489ca450edc1d9396e8dae05
Laasri Imane <imanelaasri23@gmail.com>
2025-12-08 16:47:28 +0100

    changement de utils

--- src/main/java/checkers/Utils.java
+++ src/main/java/checkers/Utils.java
@@ -1,35 +1,41 @@
 package checkers;
 
-public class Joueur {
+public class Utils {
 
-    private String nom;
-    private Piece.Color couleur;
-    private int score;
+    public static Move parseMove(String s) {
+        String[] parts = s.split("->");
+        String[] a = parts[0].trim().split(",");
+        String[] b = parts[1].trim().split(",");
 
-    public Joueur(String nom, Piece.Color couleur) {
-        this.nom = nom;
-        this.couleur = couleur;
-        this.score = 0;
+        Position from = new Position(
+                Integer.parseInt(a[0]),
+                Integer.parseInt(a[1])
+        );
+        Position to = new Position(
+                Integer.parseInt(b[0]),
+                Integer.parseInt(b[1])
+        );
+
+        return new Move(from, to);
     }
 
-    public String getNom() {
-        return nom;
-    }
+    public static void printBoard(Board b) {
+        System.out.println("   0 1 2 3 4 5 6 7 8 9");
 
-    public Piece.Color getCouleur() {
-        return couleur;
-    }
+        for (int r = 0; r < Board.SIZE; r++) {
+            System.out.print(r + "  ");
 
-    public int getScore() {
-        return score;
-    }
-
-    public void addScore(int points) {
-        this.score += points;
-    }
-
-    @Override
-    public String toString() {
-        return nom + " (" + couleur + ", score=" + score + ")";
+            for (int c = 0; c < Board.SIZE; c++) {
+                Piece p = b.getPiece(new Position(r, c));
+                if (p == null) {
+                    System.out.print(". ");
+                } else if (p.getColor() == Piece.Color.WHITE) {
+                    System.out.print("w ");
+                } else {
+                    System.out.print("b ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
